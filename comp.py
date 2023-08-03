f1969574fea4ddc04f21b6f6c8b84d94a819f7cb
dalsaemi <minjee3221@gmail.com>
2023-08-03 10:29:06 +0900

    add comp.py

---
+++ comp.py
@@ -0,0 +1,5 @@
+print('odd' if 10%2==1 else 'even')
+
+i=8
+data = 'odd' if i%2==1 else 'even'
+print(data)
--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 s = 'python'
 a,b=10,2.3
+d=3>5
 
 print(s,type(s))
 print(a,type(a))
 print(b,type(b))
+print(d,type(d))
